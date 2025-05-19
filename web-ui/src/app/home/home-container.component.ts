191e48df8014f535292f989b795cd3d24f68d877
swissarthurfreeman <swissarthurfreeman@gmail.com>
2025-05-19 16:13:28 +0200

    fix(qr-mailer:web-ui): add styling to homepage

--- web-ui/src/app/home/home-container.component.ts
+++ web-ui/src/app/home/home-container.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { DataMatrixScannerComponent } from "../data-matrix-scanner/data-matrix-scanner.component";
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home-container',
-  imports: [MatButtonModule, DataMatrixScannerComponent],
+  imports: [MatButtonModule, DataMatrixScannerComponent, TranslateModule],
   templateUrl: './home-container.component.html'
 })
 export class HomeContainerComponent {}
